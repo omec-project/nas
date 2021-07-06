2d97dd97891c56110c4563d631778d8bb96c7950
Jeremy Ronquillo <jeremyr@opennetworking.org>
2021-07-06 14:19:23 -0700

    Add Copyright and Licensing to nas Repo

--- nasConvert/UESecurityCapability.go
+++ nasConvert/UESecurityCapability.go
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2021 Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
+
 package nasConvert
 
 func UESecurityCapabilityToByteArray(buf []uint8) (nea, nia, eea, eia [2]byte) {
