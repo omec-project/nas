9c699f873dac42e402fa3c66f150ae54e7f11ac4
gab-arrobo <gabriel.arrobo@intel.com>
2024-01-23 12:47:09 -0800

    Apply gofmt to two files and remove deprecated lints (#15)

--- nasConvert/UPUInfo.go
+++ nasConvert/UPUInfo.go
@@ -13,7 +13,7 @@ import (
 	"github.com/omec-project/openapi/models"
 )
 
-//  subclause 9.11.3.53A in 3GPP TS 24.501.
+// subclause 9.11.3.53A in 3GPP TS 24.501.
 func UpuInfoToNas(upuInfo models.UpuInfo) []uint8 {
 	var buf []uint8
 
