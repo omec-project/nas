b49a7b9ac001064f0e93c3056f0fc9e72facbb54
gab-arrobo <gabriel.arrobo@intel.com>
2025-09-25 11:44:31 -0700

    Move Dnn conversion utility to NAS repository because it is primarily used there (#124)
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- nasType/comm_util.go
+++ nasType/comm_util.go
@@ -1,9 +1,24 @@
 // Copyright 2019 free5GC.org
+// SPDX-FileCopyrightText: 2025 Intel Corporation
 //
 // SPDX-License-Identifier: Apache-2.0
 //
 
 package nasType
+
+type Dnn []uint8
+
+func (d *Dnn) MarshalBinary() (data []byte, err error) {
+	data = append(data, uint8(len(*d)))
+	data = append(data, (*d)...)
+
+	return data, nil
+}
+
+func (d *Dnn) UnmarshalBinary(data []byte) error {
+	(*d) = data[1:]
+	return nil
+}
 
 // GetBitMask number, pos is shift bit
 // >= lb
