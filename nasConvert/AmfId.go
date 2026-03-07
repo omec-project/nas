9fd4a6786a884c7dda7e175ba647f44c2456abfc
Gabriel Arrobo <gabriel.arrobo@intel.com>
2026-03-06 22:36:31 -0800

    Remove dead code and tested changes using OnRamp (#168)

--- nasConvert/AmfId.go
+++ nasConvert/AmfId.go
@@ -22,9 +22,3 @@ func AmfIdToNas(amfId string) (amfRegionId uint8, amfSetId uint16, amfPointer ui
 	amfPointer = amfIdBytes[2] & 0x3f
 	return
 }
-
-func AmfIdToModels(amfRegionId uint8, amfSetId uint16, amfPointer uint8) (amfId string) {
-	tmpBytes := []uint8{amfRegionId, uint8(amfSetId>>2) & 0xff, uint8(amfSetId&0x03) + amfPointer&0x3f}
-	amfId = hex.EncodeToString(tmpBytes)
-	return
-}
